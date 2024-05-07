3f23cb8015445a6dc59e7603515ac44960e8bf7c
simon asefa <yohanness803@gmail.com>
2024-05-07 20:43:14 +0200

    fixed header

--- src/components/header/LowerHeader.jsx
+++ src/components/header/LowerHeader.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { CiMenuBurger } from "react-icons/ci";
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
+
 function LowerHeader() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className={classes.lowerHeader}>
-      <ul>
-        <li>
-          <div className={classes.menuIcon}>
-            <CiMenuBurger />
-          </div>
-          <p>All</p>
-        </li>
-        <li>today's deals</li>
-        <li>costumer service</li>
-        <li>register</li>
-        <li>gift cards</li>
-        <li>sell</li>
-      </ul>
+    <div className={classes.lowerHeaderDiv}>
+      <div className={classes.lowerHeader}>
+        <div className={classes.menuIcon} onClick={toggleMenu}>
+          <CiMenuBurger />
+        </div>
+        <div className={classes.dropdown1}>
+        <ul
+          className={menuOpen ? classes.dropdownOpen : classes.dropdownClosed}
+        >
+          <li>
+            <p>All</p>
+          </li>
+          <li>Today's Deals</li>
+          <li>Customer Service</li>
+          <li>Register</li>
+          <li>Gift Cards</li>
+          <li>Sell</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default LowerHeader
+export default LowerHeader;
--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import classes from ".//signup.module.css"
+import { Link } from 'react-router-dom'
 function Auth() {
   return (
-          <div>Signup</div>
-  )
+    <section className={classes.login}>
+      {/* logo */}
+      <Link>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHf1Bna1RlWOiy5AAicMxR41zyA64T8dRJTmS9aiZfcwJSloiF1epEShxc7A3BHYu6hA&usqp=CAU"
+          alt=""
+        />
+      </Link>
+      {/* form */}
+      <div className={classes.login_container}>
+        
+        <h1>Sign In</h1>
+        <form action="">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>  
+            <input type="password" name="password" id="password" />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+</div>
+    </section>
+  );
 }
 
 export default Auth
