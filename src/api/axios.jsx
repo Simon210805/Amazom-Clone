dc382b1b39937fffc56ec6005f7998777832cdf5
simon asefa <yohanness803@gmail.com>
2024-05-20 14:28:58 +0200

    deploying to netlify

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-ad54e/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-ad54e/us-central1/api",
+  baseURL: "https://api-scbinalbkq-uc.a.run.app/",
 });
 export  {axiosInstance}
