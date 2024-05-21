8602dc924f84be1a5afc738a7698880701d1b87a
simon asefa <yohanness803@gmail.com>
2024-05-21 18:12:43 +0200

    deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Amazon-Clone/"
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Amazon-Clone/"
   
-})
+// })
 
-export const config = {
+export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['@stripe/react-stripe-js']
-    }
-  }
-};
+      // https://rollupjs.org/configuration-options/
+      external: ["@stripe/react-stripe-js"],
+    },
+  },
+});
+
+// export const config = {
+//   build: {
+//     rollupOptions: {
+//       external: ['@stripe/react-stripe-js']
+//     }
+//   }
+// };
